20997eb86fd7772467cf69d8c0c69030fd5c3a64
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-03-29 23:56:07 +0100

    complete event code

--- src/App.js
+++ src/App.js
@@ -14,24 +14,34 @@ function App() {
     { text: " Post mail", Status: "60 % ", Deadline: "2020/04/20", id: 3 },
     { text: "Hoover carpet", Status: "100%", Deadline: "2020/04/30", id: 4 },
   ]);
+// Click on the delete button
+//   the application needs to know this is happening (listen to the event)
+//   Know which button was clicked 
+//   Remove the relevent todo object from our state 
+
+
+//Click on the complete button
+  //our application need to know this is happening 
+  // which button was clicked 
+  //update the relevent task in the state
 
   const deleteTask = (id) => {
     const filteredTasks = tasks.filter((task) => {
      return task.id !== id;
     })
 
-    setTasks(filteredTasks);
+    setTasks(filteredTasks); 
   } 
-  // const deleteCompleteTask =(id) => {
-  //   const filteredTasks = tasks.filter((task) => {
-  //     if (task.complete === complete){
-  //       return false;
-  //     } else {
-  //       return true;
-  //     }
-  //   })
-  //   setTasks(filteredTasks)
-  // }
+
+const completeTask =(id) => {
+  const newTask = tasks.map((task) => {
+    if(task.id === id && task.Status === 100 ){
+ }     
+    return task; 
+  });
+setTasks (newTask)
+}
+
 
   return (
     <div className="App">
@@ -45,6 +55,7 @@ function App() {
             return < Tasks
               key={task.id}
               deleteTaskFunc={deleteTask}
+              completeTaskFun={completeTask}
               text={task.text}
               Status={task.Status}
               Deadline={task.Deadline}
--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -4,19 +4,25 @@ import moment from 'moment';
 
 function Tasks(props) {
 
-const handleClick =() => {
+const handleDeleteClick =() => {
 // console.log("Delete button was clicked");
 // console.log(props.id);
 props.deleteTaskFunc(props.id);
 };
+
+const handleCompelteClick =() => {
+    console.log("Complete button was clicked");
+    console.log(props.id);
+    props.completeTaskFun(props.id);
+}
 
     return (
         <div className="row taskRow">
             <div className= "col-12 col-md-4">{props.text}</div>
             <div className= "col-3 col-md-2">{props.Status} </div>
             <div className= "col-3 col-md-2"> {moment(props.Deadline).format('d MM YYYY')} </div>
-            <div className= "col-3 col-md-2"><button className="btn btn-info">Complete </button></div>
-            <div className= "col-3 col-md-2"><button className="btn btn-info" onClick={handleClick}> Delete</button></div>
+            <div className= "col-3 col-md-2"><button className="btn btn-info" onClick={handleCompelteClick}>Complete </button></div>
+            <div className= "col-3 col-md-2"><button className="btn btn-info" onClick={handleDeleteClick}> Delete</button></div>
         </div>
     )
 }
