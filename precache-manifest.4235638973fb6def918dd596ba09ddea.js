e9e124f5fa09f774ce9235de05de026c17c6fee8
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-03-29 15:36:29 +0100

    Updates

---
+++ precache-manifest.4235638973fb6def918dd596ba09ddea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901a76dcaa9efe0c578dfb78a7a4172b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f37083b8d98f490f9001",
+    "url": "/static/css/main.7ef71915.chunk.css"
+  },
+  {
+    "revision": "1bd554cf97460c4b935e",
+    "url": "/static/js/2.5b8ca31d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b8ca31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37083b8d98f490f9001",
+    "url": "/static/js/main.e10a0309.chunk.js"
+  },
+  {
+    "revision": "8bfc02cd63654de6ac14",
+    "url": "/static/js/runtime-main.a4adf6dd.js"
+  }
+]);
